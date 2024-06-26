0519caf40e581470e9b58d7fb4e4a68ef5c21613
Ares Chen (from Dev Box) <arechen@microsoft.com>
2024-06-26 18:14:40 +0800

    update

--- module.psm1
+++ module.psm1
@@ -1,3 +1,13 @@
+
+class RequiredAdministratorAttribute: System.Management.Automation.CmdletBindingAttribute {
+    RequiredAdministratorAttribute():base() {
+        if (-not ([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            throw [System.Management.Automation.ValidationMetadataException] "This command requires administrative privileges"
+        }
+    }
+}
+
+
 foreach ($directory in @('Public', 'Private', '.')) {
 
     $path = Join-Path -Path $PSScriptRoot -ChildPath $directory
